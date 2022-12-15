d78fe35fcfc40b03c552510087bf914c4831247e
priobbas <pablo.rbascoy1@gmail.com>
2022-12-15 18:01:42 +0100

    Optimization changes: - js functions for change image of main character - import image from function in other js - images renamed for template string

---
+++ main-character-assets.js
@@ -0,0 +1,5 @@
+"use strict";
+
+export function image(keyCode, sprite) {
+  return `./assets/main-character-${keyCode}-${sprite}.png`;
+}
--- sprite-animation.js
+++ sprite-animation.js
@@ -1,13 +1,19 @@
 "use strict";
 
+import { image } from "./main-character-assets.js";
+
 const mainCharacter = document.getElementById("own-pixel-art-directions");
 
-let leftCounter = 1;
-let rightCounter = 1;
-let upCounter = 1;
-let downCounter = 0;
+let currentDirection = {
+  leftCounter: 1,
+  rightCounter: 1,
+  upCounter: 1,
+  downCounter: 0,
+};
+let currentDirection2 = [];
+
 const interval = 200;
-let throttle;
+let throttle = null;
 
 document.addEventListener("keyup", () => {
   if (throttle) {
@@ -16,81 +22,67 @@ document.addEventListener("keyup", () => {
   }
 });
 
+//el event listener podria estar aplicado solo a ciertas teclas, en vez de estar aplicado a cualquier tecla y despues se comprueba la tecla
 document.addEventListener("keydown", (event) => {
-  movement(event);
+  currentDirection2 = movement(event, currentDirection);
+  if (currentDirection2) {
+    currentDirection = currentDirection2;
+  }
 });
 
-function movement(event) {
+function changeImage(keyCode, counter) {
+  if (counter % 2 !== 0) {
+    mainCharacter.src = image(keyCode, 1);
+  } else if (counter % 2 === 0 && (counter / 2) % 2 !== 0) {
+    mainCharacter.src = image(keyCode, 2);
+  } else if (counter % 2 === 0 && (counter / 2) % 2 === 0) {
+    mainCharacter.src = image(keyCode, 4);
+  }
+}
+
+function movement(event, currentDirection) {
+  const { keyCode } = event;
+  let { leftCounter, rightCounter, upCounter, downCounter } = currentDirection;
+
   if (!throttle) {
-    if (event.key == "ArrowLeft") {
-      console.log("0");
-      if (leftCounter % 2 !== 0) {
-        console.log("1");
-        mainCharacter.src = "./assets/main-character-31.png";
-      } else if (leftCounter % 2 === 0 && (leftCounter / 2) % 2 !== 0) {
-        mainCharacter.src = "./assets/main-character-32.png";
-        console.log("2");
-      } else if (leftCounter % 2 === 0 && (leftCounter / 2) % 2 === 0) {
-        mainCharacter.src = "./assets/main-character-34.png";
-        console.log("3");
-      }
+    if (keyCode === 37) {
+      changeImage(keyCode, leftCounter);
       leftCounter++;
       rightCounter = 1;
       upCounter = 1;
       downCounter = 1;
-    } else if (event.key == "ArrowRight") {
-      console.log("0");
-      if (rightCounter % 2 !== 0) {
-        console.log("1");
-        mainCharacter.src = "./assets/main-character-41.png";
-      } else if (rightCounter % 2 === 0 && (rightCounter / 2) % 2 !== 0) {
-        mainCharacter.src = "./assets/main-character-42.png";
-        console.log("2");
-      } else if (rightCounter % 2 === 0 && (rightCounter / 2) % 2 === 0) {
-        mainCharacter.src = "./assets/main-character-44.png";
-        console.log("3");
-      }
-      rightCounter++;
-      leftCounter = 1;
-      upCounter = 1;
-      downCounter = 1;
-    } else if (event.key == "ArrowUp") {
-      console.log("0");
-      if (upCounter % 2 !== 0) {
-        console.log("1");
-        mainCharacter.src = "./assets/main-character-21.png";
-      } else if (upCounter % 2 === 0 && (upCounter / 2) % 2 !== 0) {
-        mainCharacter.src = "./assets/main-character-22.png";
-        console.log("2");
-      } else if (upCounter % 2 === 0 && (upCounter / 2) % 2 === 0) {
-        mainCharacter.src = "./assets/main-character-24.png";
-        console.log("3");
-      }
+    } else if (keyCode === 38) {
+      changeImage(keyCode, upCounter);
       upCounter++;
       leftCounter = 1;
       rightCounter = 1;
       downCounter = 1;
-    } else if (event.key == "ArrowDown") {
-      console.log("0");
-      if (downCounter % 2 !== 0) {
-        console.log("1");
-        mainCharacter.src = "./assets/main-character-11.png";
-      } else if (downCounter % 2 === 0 && (downCounter / 2) % 2 !== 0) {
-        mainCharacter.src = "./assets/main-character-12.png";
-        console.log("2");
-      } else if (downCounter % 2 === 0 && (downCounter / 2) % 2 === 0) {
-        mainCharacter.src = "./assets/main-character-14.png";
-        console.log("3");
-      }
+    } else if (keyCode === 39) {
+      changeImage(keyCode, rightCounter);
+      rightCounter++;
+      leftCounter = 1;
+      upCounter = 1;
+      downCounter = 1;
+    } else if (keyCode == 40) {
+      changeImage(keyCode, downCounter);
       downCounter++;
       leftCounter = 1;
       rightCounter = 1;
       upCounter = 1;
     }
-    throttle = setTimeout(() => {
-      throttle = null;
+    delayAction();
 
-      /* Your throttle interval - reduce/increase this number to suit you needs */
-    }, interval);
+    return {
+      leftCounter,
+      rightCounter,
+      upCounter,
+      downCounter,
+    };
   }
+}
+
+function delayAction() {
+  throttle = setTimeout(() => {
+    throttle = null;
+  }, interval);
 }
